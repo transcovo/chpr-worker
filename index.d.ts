a5eda7c5faf6b4c8bbf7440254737daff3134aed
Joana Costa <joana.costa@chauffeur-prive.com>
2018-11-15 10:59:30 +0100

    ENG-661 Bump Stakhanov to use its template definitions

--- index.d.ts
+++ index.d.ts
@@ -5,45 +5,17 @@
 
 /// <reference types="node" />
 
-import { Logger } from 'chpr-logger';
+
+import { Handler, Config, Process, Options as BaseOptions } from "stakhanov";
+
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
 
 declare module Worker {
-    export function createWorkers(handlers: Worker.Handler[], config: Worker.Config, options?: Worker.Options): Worker.Process
+    export function createWorkers(handlers: Handler[], config: Config, options?: Options): Process
 }
 
 declare namespace Worker {
-    export interface Process {
-        listen: () => void;
-        close: (forceExit?: Boolean) => void;
-        wait: (eventName: string, timeout?: number) => Promise<void>;
-        TASK_COMPLETED: string;
-        TASK_RETRIED: string;
-        TASK_FAILED: string;
-        WORKER_CLOSED: string;
-    }
-
-    export interface Handler<TBusMessage = any, TValidatedMessage = any> {
-        routingKey: string;
-        validate: (message: TBusMessage) => TValidatedMessage;
-        handle: (message: TValidatedMessage) => void;
-    }
-
-    export interface Config {
-        workerName: string;
-        amqpUrl: string;
-        exchangeName: string;
-        queueName: string;
-    }
-
-    export interface Options {
-        heartbeat?: number;
-        taskTimeout?: number;
-        processExitTimeout?: number;
-        channelPrefetch?: number;
-        closeOnSignals?: Boolean;
-        channelCloseTimeout?: number;
-        logger?: Logger;
-    }
+    export type Options = Omit<BaseOptions, 'logger'>
 }
 
 export = Worker;
