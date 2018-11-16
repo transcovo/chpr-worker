431b2ce0f82cb9abdb383453ed5d5bafe066e15b
Joana Costa <jcosta.pro@gmail.com>
2018-11-16 14:20:12 +0100

    ENG-661 Export all stakhanov types (#19)

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for chpr-worker 3.2.0
+// Type definitions for chpr-worker 3.2.1
 // Project: chpr-worker
 // Definitions by: Chauffeur Privé
 // TypeScript Version: 3.0.1
@@ -6,7 +6,7 @@
 /// <reference types="node" />
 
 
-import { Handler, Config, Process, Options as BaseOptions } from "stakhanov";
+import Stakhanov = require('stakhanov');
 
 type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
 
@@ -15,7 +15,10 @@ declare module Worker {
 }
 
 declare namespace Worker {
-    export type Options = Omit<BaseOptions, 'logger'>
+    export type Process = Stakhanov.Process
+    export type Handler = Stakhanov.Handler
+    export type Config = Stakhanov.Config
+    export type Options = Omit<Stakhanov.Options, 'logger'>
 }
 
 export = Worker;
